683b83b79209b5014fdde7a380b6f7b082857d5f
Reck Hou <reckhou@gmail.com>
2015-03-29 12:18:15 +0800

    Initial commit.

---
+++ Assets/CharacterAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAction : MonoBehaviour {
+	public float RotateSpeed; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Move(Vector3 destination) {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = destination;
+	}
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+	public GameObject DestIndicator;
+	public GameObject Character;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 mousePos = Input.mousePosition;
+			Ray ray = Camera.main.ScreenPointToRay(mousePos);
+			RaycastHit[] hitArray = (Physics.RaycastAll(ray));
+			if (hitArray.Length > 0) {
+				// only the ground is possible to be hit in this case
+				Vector3 hitPoint = hitArray[0].point;
+				DestIndicator.transform.position = hitPoint;
+				Character.GetComponent<CharacterAction>().Move(hitPoint);
+			}
+		}
+	}
+}
